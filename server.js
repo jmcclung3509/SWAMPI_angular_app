b1f784472c38750d872ede38e6d94bf4cb0c68ea
Jessica Turner <jmcclung3509@gmail.com>
2022-08-14 22:14:20 -0700

    Removed asteric from .get

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.static(__dirname + '/dist/SWAMPI_ANGULAR_APP'));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/SWAMPI_ANGULAR_APP/index.html'));
 });
 
