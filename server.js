f120def977fad4e1bcfe9dc41c2b23c3155789c2
Jessica Turner <jmcclung3509@gmail.com>
2022-08-17 16:29:00 -0700

    Created dist folder, starwars folder added dist contents to starwars folder, changed - to _

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/star-wars-app"));
+app.use(express.static(__dirname + "/dist/star_wars_app"));
 
 app.get("/*",function (req,res) {
-    res.sendFile(path.join(__dirname + "/dist/star-wars-app/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/star_wars_app/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
