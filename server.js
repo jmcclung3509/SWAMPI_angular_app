deea10caf5de61a9c407c7358bf4d58aa5a14a95
Jessica Turner <jmcclung3509@gmail.com>
2022-08-17 11:20:56 -0700

    Added spaces around binary operators

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 //Install express server
-const express=require("express");
-const path=require("path");
+const express = require("express");
+const path = require("path");
 
-const app=express();
+const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+"/dist/star-wars-app"));
+app.use(express.static(__dirname + "/dist/star-wars-app"));
 
 app.get("/*",function (req,res) {
-    res.sendFile(path.join(__dirname+"/dist/star-wars-app/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/star-wars-app/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT||8081,function () {
+app.listen(process.env.PORT || 8081,function () {
     console.log("Listening on port");
 });
